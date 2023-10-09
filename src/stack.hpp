2caedae4e6c2aaef0e9487212d40cdfa3ed55852
GeorginRMZ <87065916+GeorginRMZ@users.noreply.github.com>
2023-10-09 10:58:12 +0300

    Added files

--- src/stack.hpp
+++ src/stack.hpp
@@ -14,23 +14,16 @@ public:
     Stack(int size) : stackSize(size), arr(new T[stackSize]), pointer(0), lastError(new char[ERROR_SIZE]) {}
     Stack() : stackSize(DEFAULT_STACK_SIZE), arr(new T[stackSize]), pointer(0), lastError(new char[ERROR_SIZE]) {}
 
-    ~Stack() {
-	delete[] this->arr;
-    }
+    ~Stack() { delete[] this->arr; }
 
-    char* getLastError() {
-	return this->lastError;
-    }
+    char* getLastError() { return this->lastError; }
 
-    bool isFull() {
-	return this->pointer >= stackSize;
-    }
+    bool isFull() { return this->pointer >= stackSize; }
 
     void push(T value) {
 	if (isFull()) {
 	    error(STACK_FULL_ERROR);
-	}
-	else {
+	} else {
 	    arr[this->pointer++] = value;
 	}
     }
@@ -40,28 +33,19 @@ public:
 	    this->pointer = 0;
 	    error(POINTER_ERROR);
 	    return NULL;
-	}
-	else {
+	} else {
 	    return arr[--this->pointer];
 	}
     }
 
-    T peek() {
-	return arr[this->pointer - 1];
-    }
+    T peek() { return arr[this->pointer - 1]; }
 
-    int size() {
-	return this->pointer;
-    }
+    int size() { return this->pointer; }
 
-    bool isEmpty() {
-	return this->pointer == 0;
-    }
+    bool isEmpty() { return this->pointer == 0; }
 
 private:
-    void error(const char* err) {
-	this->lastError = const_cast<char*>(err);
-    }
+    void error(const char* err) { this->lastError = const_cast<char*>(err); }
     T* arr;
     char* lastError;
     int pointer;
